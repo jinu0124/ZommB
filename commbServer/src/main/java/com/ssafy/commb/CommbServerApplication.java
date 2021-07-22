20043ea6cb782643fe5790eeff734e0c9e990258
MoonGaNae <anstjd9115@gmail.com>
2021-07-22 13:54:58 +0900

    Server Project Init

---
+++ commbServer/src/main/java/com/ssafy/commb/CommbServerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.commb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommbServerApplication.class, args);
+    }
+
+}
---
+++ commbServer/src/test/java/com/ssafy/commb/CommbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.commb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommbServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
