337b405de3dade0e6e2df8c7ac8e2d187d6492c5
이세라 <you@example.com>
2021-07-22 16:35:16 +0900

    [#S05P12A602-349] Docs: 포트 변경 및 dir 구분

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import book from './modules/book'
+import challenge from './modules/challenge'
+import error from './modules/error'
+import feed from './modules/feed'
+import search from './modules/search'
+import user from './modules/user'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  plugins: [createPersistedState()],
   modules: {
+    book,
+    challenge,
+    error,
+    feed,
+    search,
+    user
   }
 })
