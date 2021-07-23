0ea8800c4e1fc5e46ad0ecab6792b70786fe2b65
이세라 <you@example.com>
2021-07-23 16:38:47 +0900

    [S05P12A602-474] Feat: Header 생성 + App.vue에 등록

---
+++ client/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header">
+    <div class="d-flex justify-content-between align-items-center">
+      <i 
+        class="nav-toggle fas fa-water"
+        type="button" 
+        data-bs-toggle="offcanvas" 
+        data-bs-target="#offcanvasWithBothOptions" 
+        aria-controls="offcanvasWithBothOptions"
+      ></i>
+      <span class="header-logo">CommB</span>
+      <div class="dropdown">
+        <img 
+          class="user-profile dropdown-toggle"
+          type="button" 
+          id="UserMenuDropdown"
+          data-bs-toggle="dropdown" 
+          aria-expanded="false"
+          src="@/assets/image/test/profileTest.jpg" 
+          alt=""
+        >
+        <HeaderUserMenu/>
+      </div>
+    </div>
+    <HeaderSideNav/>
+  </div>
+</template>
+
+<script>
+import HeaderSideNav from "./HeaderSideNav.vue"
+import HeaderUserMenu from "./HeaderUserMenu.vue"
+
+export default {
+  name: 'Header',
+  components: {
+    HeaderSideNav,
+    HeaderUserMenu,
+  }
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #7B60F1;
+    padding: 12px 20px;
+  }
+  .nav-toggle {
+    font-size: 1.5rem;
+    color: #fff;
+  }
+  .header-logo {
+    font-family: 'Black Han Sans', sans-serif;
+    font-size: 1.5rem;
+    color: #fff;
+  }
+  .user-profile {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 100%;
+  }
+  
+</style>
---
+++ client/src/components/HeaderUserMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="dropdown-menu" aria-labelledby="UserMenuDropdown">
+    <li><a class="dropdown-item" href="#">내 프로필</a></li>
+    <li><a class="dropdown-item" href="#">계정 관리</a></li>
+    <li><a class="dropdown-item" href="#">로그아웃</a></li>
+    <li><a class="dropdown-item" href="#">회원 탈퇴</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'HeaderUserMenu',
+}
+</script>
+
+<style scoped>
+  .dropdown-menu {
+    border: none;
+    background: rgba(0, 0, 0, 0.7);
+    border-radius: 10px 0px 10px 10px;
+    min-width: 3rem;
+  }
+  .dropdown-item {
+    color: #fff;
+    font-size: 0.9rem;
+    font-weight: 300;
+  }
+</style>
