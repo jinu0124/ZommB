a5141dee15eff6f1e9d16de794c30cd3fdfc1162
이세라 <you@example.com>
2021-07-26 15:52:57 +0900

    [S05P12A602-383] Feat: 로그인 페이지 구성

--- client/src/views/user/Login.vue
+++ client/src/views/user/Login.vue
@@ -1,15 +1,263 @@
 <template>
   <div class="login">
-    LOGIN PAGE
+    <UnauthorizedHeader/>
+    <div class="login-header">
+      <div class="title">Login</div>
+      <div class="description">반가워요! 로그인 후 CommB의 새로운 소식을 만나보세요!</div>
+    </div>
+    <div class="account-form p-5 d-flex flex-column justify-content-center align-items-center">
+      <img class="account-deco" src="@/assets/image/deco/accountDeco.svg" alt="accountDeco">
+      
+      <div class="account-inputs">
+        <!-- 이메일 input -->
+        <div class="account-input-box">
+          <input
+            id="email"
+            class="account-input"
+            v-model="email"
+            type="text"
+            @keyup.enter="onLogin"
+            autocapitalize="off"
+            required
+          />
+            <label>이메일 계정</label>
+          <div class="error-text" v-if="error.email">{{error.email}}</div>
+        </div>
+        <!-- 비밀번호 input -->
+        <div class="account-input-box mt-2">
+          <input
+            id="password"
+            class="account-input"
+            v-model="password"
+            type="password"
+            @keyup.enter="onLogin"
+            required
+          />
+            <label>비밀번호</label>
+          <div class="error-text" v-if="error.password">{{error.password}}</div>
+        </div>
+        <!-- 로그인 버튼 -->
+        <button
+          class="btn-2 btn-yellow mt-4"
+          @click="onLogin"
+        >로그인</button>
+      </div>
+
+      <div class="move-btns my-2">
+        <span
+          class="btn-text-s"
+          type="button"
+          @click="moveToSignup"
+        >회원가입</span>
+        <span class="btn-text-s"> | </span>
+        <span
+          class="btn-text-s"
+          type="button"
+        >비밀번호 찾기</span>
+      </div>
+
+      <div class="social-login">
+        <img class="img-fluid my-3" src="@/assets/image/deco/or.svg" alt="">
+        <div class="d-flex gap-3 justify-content-center">
+          <button class="kakao-btn">
+            <img src="@/assets/image/deco/kakao.png" alt="">
+          </button>
+          <button class="google-btn">
+            <img src="@/assets/image/deco/google.png" alt="">
+          </button>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import PV from "password-validator"
+import * as EmailValidator from "email-validator"
+import { mapActions } from "vuex"
+import UnauthorizedHeader from '@/components/user/UnauthorizedHeader'
+
 export default {
   name: 'Login',
+  components: {
+    UnauthorizedHeader
+  },
+  data: () => {
+    return {
+      email: "",
+      password: "",
+      passwordSchema: new PV(),
+      error: {
+        email: false,
+        passowrd: false
+      },
+      isSubmit: false,
+    }
+  },
+  methods: {
+    ...mapActions('user', ['moveToSignup']),
+    checkForm() {
+      if (this.email.length >= 0 && !EmailValidator.validate(this.email))
+        this.error.email = "이메일 형식이 아닙니다.";
+      else this.error.email = false;
+
+      if (
+        this.password.length >= 0 &&
+        !this.passwordSchema.validate(this.password)
+      )
+        this.error.password = "영문,숫자 포함 8 자리이상이어야 합니다.";
+      else this.error.password = false;
+
+      let isSubmit = true;
+      Object.values(this.error).map(v => {
+        if (v) isSubmit = false;
+      });
+      this.isSubmit = isSubmit;
+    },
+    onLogin() {
+      // 백엔드 되면 작성
+    },
+
+  },
+  created() {
+    this.passwordSchema
+      .is()
+      .min(8)
+      .is()
+      .max(100)
+      .has()
+      .digits()
+      .has()
+      .letters();
+  },
+  watch: {
+    password: function() {
+      this.checkForm();
+    },
+    email: function() {
+      this.checkForm();
+    }
+  },
 }
 </script>
 
-<style>
+<style src="@/assets/style/button.css"></style>
+<style scoped>
+  .login {
+    display: flex;
+    flex-flow: column;
+    height: 100%;
+    min-height: 100%;
+  }
+  .login-header {
+    margin: 65px 20px 20px;
+    flex: 0;
+  }
+  .login-header .title {
+    font-family: 'Black Han Sans', sans-serif;
+    font-size: 2.5rem;
+    color: #fff;
+    text-shadow: 2px 2px #683EC9;
+  }
+  .login-header .description {
+    color: #fff;
+    font-weight: 300;
+    font-size: 0.7rem;
+  }
+  .account-form {
+    flex: 1;
+    position: relative;
+    background-color: #fff;
+    margin-left: 20px;
+    border-radius: 30px 0px 0px 0px;
+  }
+  .account-deco {
+    width: 45px;
+    height: 60px;
+    position: absolute;
+    top: -5px;
+    left: 25px;
+  }
+
+  .account-input-box {
+    position: relative;
+  }
+
+  .account-input {
+    width: 210px !important;
+    height: 35px;
+    margin-top: 15px;
+    background-color: #F1F1F1;
+    color: #212121;
+    box-shadow: none;
+    border: none;
+    outline: none;
+    height: 35px;
+    padding-left: 20px;
+    font-size: 0.875rem;
+    border-radius: 20px;
+    transition: none;
+  }
+  .account-input ~ label{
+    position: absolute;
+    color: #A4A4A4;
+    font-size: 0.875rem;
+    left: 20px;
+    top: 22px;
+    transition: 0.4s ease all;
+    pointer-events:none;
+  }
+  .account-input:focus {
+    background-color: #F1F1F1;
+    box-shadow: none;
+    border: none;
+    outline: none;
+  }
+  .account-input:focus ~ label,
+  .account-input:valid ~ label {
+    top: -5px; 
+    font-size: 0.7rem; 
+    color: #585858; 
+    transition: 0.4s ease all;
+    pointer-events:none;
+  }
+  .error-text {
+    padding-left: 20px;
+    color: #FF7777;
+    font-size: 0.75rem;
+    font-weight: 400;
+  }
+
+  .social-login {
+    width: 210px;
+  }
+
+  .social-login button {
+    width: 45px;
+    height: 45px;
+    border-radius: 100%;
+    border: none;
+    outline: none;
+  }
+
+  .kakao-btn {
+    background-color: #FFD600;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  .google-btn {
+    background-color: #fff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  
+  .social-login button img {
+    max-width: 70%;
+    height: auto;
+  }
+
+
+
+  
 
 </style>
