678e149b8b87bc769aa669f89b8e9a5165da4a77
이세라 <you@example.com>
2021-07-26 15:57:52 +0900

    [S05P12A602-383] Feat & Design: 비회원 헤더 구성 + css 틀 수정

---
+++ client/src/components/user/UnauthorizedHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header fixed-top">
+    <div class="d-flex align-items-center">
+      <i 
+        class="back-btn fi-rr-angle-small-left"
+        @click="$router.go(-1)"
+      ></i>
+      <span class="header-logo">CommB</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UnauthorizedHeader',
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #7B60F1;
+    padding: 12px 20px;
+    height: 60px;
+  }
+  .back-btn {
+    font-size: 1.5rem;
+    color: #fff;
+  }
+  .header-logo {
+    font-family: 'Black Han Sans', sans-serif;
+    font-size: 1.5rem;
+    margin: 0 auto;
+    color: #fff;
+  }
+  
+</style>
