27d792e27fa689280af9a6ebda5c5c9337933203
jinu0124 <jinwoo6612@naver.com>
2021-07-26 11:34:22 +0900

    Feat: FeedController.java 더미 생성

--- commbServer/src/main/java/com/ssafy/commb/controller/FeedController.java
+++ commbServer/src/main/java/com/ssafy/commb/controller/FeedController.java
@@ -4,35 +4,60 @@ import com.ssafy.commb.dto.book.BookDto;
 import com.ssafy.commb.dto.feed.CommentDto;
 import com.ssafy.commb.dto.feed.FeedDto;
 import com.ssafy.commb.dto.feed.HashTagDto;
+import com.ssafy.commb.dto.report.ReportDto;
+import com.ssafy.commb.dto.user.MyDto;
 import com.ssafy.commb.dto.user.UserDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static java.lang.System.currentTimeMillis;
 
 @RestController
 @RequestMapping(value="/feeds")
 public class FeedController {
 
+    static final int id = 1;
+    static final String nickname = "크루엘라";
+    static final String url = "URL 자리";
+    static final String tag = "#오늘도 힘내세요";
+    static final String content = "이것은 글입니다.";
+    static final int cnt = 134;
+    static Date date;
+    static final boolean bool = true;
+
+    static {
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd HH:MM:SS").parse("2021-07-31 10:12:15");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // /feeds?searchWord="abc"
     // 게시물 리스트 검색
     @GetMapping("")
-    public Object getFeeds(@RequestParam String searchWord){
-        UserDto user = UserDto.builder().id(3).nickname("nick").userFileUrl("rehert").build();
+    public Object getFeeds(@RequestParam String searchWord) throws ParseException {
+        UserDto user = UserDto.builder().id(id).nickname(nickname).userFileUrl(url).build();
         BookDto book = new BookDto();
 
         List<HashTagDto> hashTags = new ArrayList<>();
-        hashTags.add(HashTagDto.builder().tag("hre").build());
+        hashTags.add(HashTagDto.builder().tag(tag).build());
 
         List<CommentDto> comments = new ArrayList<>();
-        comments.add(CommentDto.builder().id(12).content("geww").userId(45).nickname("nick2").thumbCnt(560).createAt(new Date("2021-07-31")).isThumb(false).isMod(false).build());
+        comments.add(CommentDto.builder().id(id).content(content).userId(id).nickname(nickname).thumbCnt(cnt).createAt(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-31 12:10:00")).isThumb(bool).isMod(bool).build());
 
 
-        FeedDto feed = FeedDto.builder().id(1).createAt(new Date("2021-07-31")).content("few").isThumb(true)
-                .thumbCnt(565).feedFileUrl("fwergrwe").user(user).book(book).hashTags(hashTags).comments(comments).build();
+        FeedDto feed = FeedDto.builder().id(id).createAt(new SimpleDateFormat("yyyy-MM-dd HH:MM:SS").parse("2021-07-31 10:12:15")).content(content).isThumb(bool)
+                .thumbCnt(cnt).feedFileUrl(url).user(user).book(book).hashTags(hashTags).comments(comments).build();
 
         FeedDto.Response feedRes = new FeedDto.Response();
         feedRes.setData(feed);
@@ -45,70 +70,116 @@ public class FeedController {
 
     // 게시물 작성
     @PostMapping("")
-    public Object uploadFeed(@RequestBody Object feed){
-        return null;
+    public ResponseEntity uploadFeed(@RequestBody FeedDto feed, MultipartHttpServletRequest request){
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     // 게시물 수정
     @PutMapping("/{feedId}")
-    public Object modifyFeed(@RequestBody Object feed, @PathVariable String feedId){
-        return null;
+    public ResponseEntity modifyFeed(@RequestBody FeedDto feed, @PathVariable String feedId){
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     // 게시물 삭제
     @DeleteMapping("/{feedId}")
-    public Object deleteFeed(@PathVariable String feedId){
-        return null;
+    public ResponseEntity deleteFeed(@PathVariable String feedId){
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     // 게시물 좋아요 or 취소
     @PostMapping("/{feedId}/feed-like")
-    public Object likeFeed(@PathVariable String feedId){
-        return null;
+    public ResponseEntity likeFeed(@PathVariable String feedId){
+
+        return new ResponseEntity(HttpStatus.valueOf(201));
     }
 
+    @DeleteMapping("/{feedId}/feed-like")
+    public ResponseEntity deleteLikeFeed(@PathVariable String feedId){
+
+        return new ResponseEntity(HttpStatus.valueOf(204));
+    }
+
+    // /feeds/5/feed-likes
     // 게시물 좋아요 목록
     @GetMapping("/{feedId}/feed-likes")
-    public Object likeFeeds(@PathVariable String feedId){
-        return null;
+    public ResponseEntity<MyDto.Response> likeFeeds(@PathVariable String feedId){
+        MyDto.Response myRes = new MyDto.Response();
+        MyDto my = MyDto.builder().id(id).nickname(nickname).userFileUrl(url).isFollow(bool).build();
+
+        myRes.setData(my);
+        return new ResponseEntity<MyDto.Response>(myRes, HttpStatus.OK);
     }
 
     // 댓글 작성
     @PostMapping("/{feedId}/comments")
-    public Object uploadComment(@PathVariable String feedId, @RequestBody Object feed){
-        return null;
+    public ResponseEntity uploadComment(@PathVariable String feedId, @RequestBody FeedDto feed){
+
+        return new ResponseEntity(HttpStatus.valueOf(201));
     }
 
 
     // 댓글 수정
     @PutMapping("/{feedId}/comments/{commentId}")
-    public Object modifyComment(@PathVariable String commentId, @PathVariable String feedId, @RequestBody Object feed){
-        return null;
+    public ResponseEntity modifyComment(@PathVariable String commentId, @PathVariable String feedId, @RequestBody FeedDto feed){
+
+        return new ResponseEntity(HttpStatus.valueOf(201));
     }
 
     // 댓글 삭제
     @DeleteMapping("/{feedId}/comments/{commentId}")
-    public Object deleteComment(@PathVariable String commentId, @PathVariable String feedId){
-        return null;
+    public ResponseEntity deleteComment(@PathVariable String commentId, @PathVariable String feedId){
+
+        return new ResponseEntity(HttpStatus.valueOf(204));
     }
 
     // 댓글 좋아요 or 취소
     @PostMapping("/{feedId}/comments/{commentId}/comment-like")
-    public Object likeComment(@PathVariable String feedId, @PathVariable String commentId){
-        return null;
+    public ResponseEntity likeComment(@PathVariable String feedId, @PathVariable String commentId){
+
+        return new ResponseEntity(HttpStatus.valueOf(201));
+    }
+
+    @DeleteMapping("/{feedId}/comments/{commentId}/comment-like")
+    public ResponseEntity deleteLikeComment(@PathVariable String feedId, @PathVariable String commentId){
+
+        return new ResponseEntity(HttpStatus.valueOf(204));
+    }
+
+    // /feeds/5/following/feeds
+    // 내가 팔로잉하는 사람들의 피드 목록
+    @GetMapping("/{userid}/following/feeds")
+    public Object getFollowingFeeds(@PathVariable String userid) throws ParseException {
+        UserDto user = UserDto.builder().id(id).nickname(nickname).userFileUrl(url).build();
+        BookDto book = new BookDto();
+
+        List<HashTagDto> hashTags = new ArrayList<>();
+        hashTags.add(HashTagDto.builder().tag(tag).build());
+
+        List<CommentDto> comments = new ArrayList<>();
+        comments.add(CommentDto.builder().id(id).content(content).userId(id).nickname(nickname).thumbCnt(cnt).createAt(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-31 12:10:00")).isThumb(bool).isMod(bool).build());
+
+
+        FeedDto feed = FeedDto.builder().id(id).createAt(new SimpleDateFormat("yyyy-MM-dd HH:MM:SS").parse("2021-07-31 10:12:15")).content(content).isThumb(bool)
+                .thumbCnt(cnt).feedFileUrl(url).user(user).book(book).hashTags(hashTags).comments(comments).build();
+
+        FeedDto.Response feedRes = new FeedDto.Response();
+        feedRes.setData(feed);
+
+        List<FeedDto.Response> feedResList = new ArrayList<>();
+        feedResList.add(feedRes);
+
+        return new ResponseEntity<List<FeedDto.Response>>(feedResList, HttpStatus.OK);
     }
 
 
-    // 댓글 좋아요 취소 삭제
-//    @DeleteMapping("/{feedId}/comments/{commentId}/comment-like")
-//    public Object deleteLikeComment(@PathVariable String feedId, @PathVariable String commentId){
-//        return null;
-//    }
-
     // 피드 신고
     @PostMapping("/{feedId}/reports")
-    public Object reportFeed(@PathVariable String feedId, @RequestBody Object report){
-        return null;
+    public ResponseEntity reportFeed(@PathVariable String feedId, @RequestBody ReportDto report){
+
+         return new ResponseEntity(HttpStatus.valueOf(201));
     }
 
 
