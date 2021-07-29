f88d1f332b2c0eef0091a1d675995145cb4330bb
MoonGaNae <anstjd9115@gmail.com>
2021-07-29 17:54:06 +0900

    Fix : POST login 403 에러에 따른 csrf disable

--- commbServer/src/main/java/com/ssafy/commb/config/security/WebSecurityConfig.java
+++ commbServer/src/main/java/com/ssafy/commb/config/security/WebSecurityConfig.java
@@ -30,7 +30,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .anyRequest().permitAll()
                 .and()
                 .oauth2Login()
