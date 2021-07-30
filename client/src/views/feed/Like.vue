a865f63198d44fb2451abc0c7f316125ee3eefaf
박재준 <wowns30@naver.com>
2021-07-30 11:37:47 +0900

    Design : Reply + Like 레이아웃 추가

---
+++ client/src/views/feed/Like.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="like">
+    <LikeListHeader/>
+    <LikeList/>
+  </div>
+</template>
+
+<script>
+import LikeListHeader from '@/components/feeds/like/LikeListHeader.vue'
+import LikeList from '@/components/feeds/like/LikeList.vue'
+
+export default {
+  name: 'Like',
+  components: {
+    LikeListHeader,
+    LikeList
+  },
+  data() {
+    return {};
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
