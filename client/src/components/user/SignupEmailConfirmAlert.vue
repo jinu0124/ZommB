a6d94fcc792bb0c04152628608225232462ba28d
이세라 <you@example.com>
2021-07-30 20:14:27 +0900

    [S05P12A602-378] Feat: 이메일 중복확인 관련 알림창 생성

---
+++ client/src/components/user/SignupEmailConfirmAlert.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="email-check d-flex flex-column align-items-center justify-content-center">
+    <div class="content d-flex align-items-center">
+      <img src="@/assets/image/camel/camelSmile.svg" alt="">
+      <div class="text">
+        사용 가능한 <br/>
+        이메일입니다.
+      </div>
+    </div>
+    <button
+      class="btn-4 btn-yellow mt-2"
+      @click="requestClose"
+    >OK!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignupEmailConfirmAlert',
+  methods: {
+    requestClose: function () {
+      this.$emit('ok')
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .email-check {
+    width: 240px;
+    height: 150px;
+    background-color: #97DFFC;
+    border-radius: 15px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+  }
+  .email-check .content img {
+    width: 70px;
+  }
+  .email-check .content .text {
+    font-weight: 600;
+    color: #212121;
+  }
+</style>
--- client/src/components/user/SignupEmailRejectAlert.vue
+++ client/src/components/user/SignupEmailRejectAlert.vue
@@ -1,37 +1,43 @@
 <template>
-  <div class="SignupEmailCheckAlert d-flex flex-column align-items-center justify-content-center">
+  <div class="email-check d-flex flex-column align-items-center justify-content-center">
     <div class="content d-flex align-items-center">
-      <img src="@/assets/image/camel/camelSmile.svg" alt="">
-      <div class="text">
-        사용 가능한 <br/>
+      <img src="@/assets/image/camel/camelCrying.svg" alt="">
+      <div class="text ms-2">
+        이미 사용 중인<br/>
         이메일입니다.
       </div>
     </div>
     <button
       class="btn-4 btn-yellow mt-2"
+      @click="requestClose"
     >OK!</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SignupEmailCheckAlert',
+  name: 'SignupEmailRejectAlert',
+  methods: {
+    requestClose: function () {
+      this.$emit('ok')
+    }
+  }
 
 }
 </script>
 
 <style scoped>
-  .SignupEmailCheckAlert {
+  .email-check {
     width: 240px;
     height: 150px;
     background-color: #97DFFC;
     border-radius: 15px;
     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
   }
-  .SignupEmailCheckAlert .content img {
+  .email-check .content img {
     width: 70px;
   }
-  .SignupEmailCheckAlert .content .text {
+  .email-check .content .text {
     font-weight: 600;
     color: #212121;
   }
