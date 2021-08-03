e1b2a33a54dce7e435319fa1e2fe040b108f81dc
MoonGaNae <anstjd9115@gmail.com>
2021-08-03 16:50:27 +0900

    Feat : follower, follwoing 유저를 기준으로 FindFollow 메서드 추가

--- commbServer/src/main/java/com/ssafy/commb/repository/FollowRepository.java
+++ commbServer/src/main/java/com/ssafy/commb/repository/FollowRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.commb.repository;
 
+import com.ssafy.commb.model.User;
 import com.ssafy.commb.model.follow.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
+
+    public Optional<Follow> findByFollowerAndFollowing(User follower, User following);
 }
