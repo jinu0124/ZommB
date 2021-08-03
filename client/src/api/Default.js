801d83506ae30bbe74cdc21d7e7aded3cd29cada
이세라 <you@example.com>
2021-08-03 10:02:22 +0900

    Feat: API 인터셉터 + 기본 account 요청

--- client/src/api/Default.js
+++ client/src/api/Default.js
@@ -1,12 +1,23 @@
 import axios from 'axios'
 import router from '@/router'
+import user from '@/store/modules/user'
 
+// 저장된 토큰 불러오기 (1) localStorage
+// let accessToken = JSON.parse(localStorage.getItem('vuex')).user.accessToken
+// let refreshToken = JSON.parse(localStorage.getItem('vuex')).user.refreshToken
+
+// 저장된 토큰 불러오기 (2) vuex
+let accessToken = user.state.accessToken
+// let refreshToken = user.state.refreshToken
 
 const _axios = axios.create({
-  baseURL: 'http://i5a602.p.ssafy.io:8080',
-  timeout: 1000,
+  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://i5a602.p.ssafy.io:8080',
+  timeout: 10000,
   // 헤더 정보 자동 추가
-  // headers: {'access-token': 'foobar'}
+  headers: {
+    'access-token': accessToken
+  }
 })
 
 /*
@@ -39,6 +50,8 @@ _axios.interceptors.response.use(
 
   function (error) {
     // 1. 토큰 만료 시, 토큰 refresh (jwt 정리되면 추가)
+    // 100 > 액세스 토큰 만료
+    // 액세스 + 리프레쉬 보내면 > 액세스 새로 줌
     // 2. 서버 에러 처리
     if (error.response.status >= 500) {
       router.push({ name: 'ServerError'})
--- client/src/api/user.js
+++ client/src/api/user.js
@@ -2,9 +2,13 @@ import _axios from "./Default"
 
 export default {
   // user
-  // login(userData) {
-  //   return axios.post(`${SERVER_URL}/users/login`, userData)
-  // },
+  login(userData) {
+    return _axios({
+      url: '/users/login',
+      method: 'post',
+      data: userData
+    })
+  },
   signup(userData) {
     return _axios({
       url: '/users',
@@ -26,4 +30,24 @@ export default {
       data: userData
     })
   },
+  updateInfo(userId, userData) {
+    return _axios({
+      url: `users/${userId}`,
+      method: 'post',
+      data: userData,
+    })
+  },
+  changePassword(userId, userData) {
+    return _axios({
+      url: `users/${userId}`,
+      method: 'patch',
+      data: userData
+    })
+  },
+  withdrawal(userId) {
+    return _axios({
+      url: `users/${userId}`,
+      method: 'delete',
+    })
+  }
 }
