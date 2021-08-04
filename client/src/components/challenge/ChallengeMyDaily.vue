b137f40e62cf3fea22ef79168ec3489f6b1f206a
이세라 <you@example.com>
2021-08-04 14:34:36 +0900

    [S05P13A602-225] Design: 내 챌린지 기본 구성

---
+++ client/src/components/challenge/ChallengeMyDaily.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my-daily-challenge my-2">
+    <div class="my-daily-header">
+      <div class="title">Daily Keywords</div>
+      <div class="subtitle">{{ dailyTotal }}개의 책린지 중 {{ dailyMy }}개에 참여하셨네요!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'ChallengeMyDaily',
+  data () {
+    return {
+      dailyMy: 2,
+
+    }
+  },
+  computed: {
+    dailyTotal () {
+      return moment().format('D')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .my-daily-header .title {
+    font-size: 20px;
+    font-weight: 700;
+  }
+  .my-daily-header .subtitle {
+    font-size: 12px;
+    padding-left: 5px;
+  }
+</style>
