a5d247917beeb14a22b764f325ec2dbd0d182583
jinu0124 <jinwoo6612@naver.com>
2021-08-04 13:37:39 +0900

    Refactor : UserController.java (비밀번호 변경, 소셜 남음)

--- commbServer/src/main/java/com/ssafy/commb/controller/UserController.java
+++ commbServer/src/main/java/com/ssafy/commb/controller/UserController.java
@@ -25,42 +25,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/users")
 @Api("User Controller API V1")
 public class UserController {
-
-
-    // Dummy Data Set----------------------------------------------------------------------
-    static final int id = 1;
-    static final String nickname = "크루엘라";
-    static final String url = "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F484319%3Ftimestamp%3D20201124225204";
-    static final String tag = "#오늘도 힘내세요";
-    static final String content = "이것은 글입니다.";
-    static final int cnt = 134;
-    static Date date;
-    static final boolean bool = true;
-    static final String name = "트럼프";
-    static final String genre = "공포";
-    static final int year = 2021;
-    static final float rate = 3.5f;
-    static final String keyword = "키워드입니다";
-    static final int bookmark = 4;
-    static final int pencil = 2;
-
-    static {
-        try {
-            date = new SimpleDateFormat("yyyy-MM-dd HH:MM:SS").parse("2021-07-31 10:12:15");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-    // Dummy Data Set----------------------------------------------------------------------
-
     @Autowired
     private ProfileService profileService;
 
@@ -85,7 +56,6 @@ public class UserController {
     @Value("${security.refreshtoken}")
     private String refreshToken;
 
-    // @
     // 회원관리(관리자) - (관리자)가 회원 정보 리스트 검색
     @GetMapping("")
     @ApiOperation(value = "(관리자)회원 정보 리스트 검색", response = UserDto.Response.class)
@@ -106,7 +76,6 @@ public class UserController {
 //        return new ResponseEntity<UserDto.ResponseList>(userResList, HttpStatus.OK);
 //    }
 
-    // @
     // 회원가입/로그인 - 자체 회원가입
     @PostMapping("")
     @ApiOperation(value = "자체 회원가입")
@@ -122,7 +91,6 @@ public class UserController {
         return new ResponseEntity<>(map, HttpStatus.valueOf(201));
     }
 
-    // @
     // 회원가입/로그인 - Email 중복 확인
     @GetMapping("/email")
     @ApiOperation(value = "Email 중복 확인")
@@ -133,7 +101,6 @@ public class UserController {
         return new ResponseEntity(HttpStatus.valueOf(200));
     }
 
-    // @
     // 회원가입/로그인 - Email 인증
     @PostMapping("/confirm-email")
     @ApiOperation(value = "Email 인증")
@@ -144,7 +111,6 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
-    // @
     @GetMapping("/checkEmailComplete")
     @ApiOperation(value = "Email 인증 확인")
     public String checkEmailComplete(@RequestParam String key, RedirectAttributes redirect){
@@ -152,10 +118,10 @@ public class UserController {
 //            RedirectUrlBuilder redirectUrl = new RedirectUrlBuilder();
 //            redirectUrl.setContextPath("index.html");
             // Redirect를 어떻게 시키지.. 그냥 빈페이지에 인증되었다고만 적어두어도 괜찮나..??
-            return "인증이 완료되었습니다. 돌아가! 로그인해!";
+            return "인증이 완료되었습니다. 감사합니다.";
         }
 
-        return "이메일 인증을 위한 토큰이 만료되었거나 유효하지 않아 인증에 실패했대요~";
+        return "메일 인증을 위한 토큰이 만료되었거나 유효하지 않아 인증에 실패하였습니다.";
     }
 
     // 회원가입/로그인 - 소셜 회원가입
@@ -163,14 +129,10 @@ public class UserController {
     @ApiOperation(value = "소셜 회원가입", response = MyDto.Response.class)
     public ResponseEntity<MyDto.Response> kakaoLogin() {
 
-        MyDto my = MyDto.builder().id(1).nickname(nickname).userFileUrl(url).build();
-        MyDto.Response myRes = new MyDto.Response();
-        myRes.setData(my);
 
-        return new ResponseEntity<MyDto.Response>(myRes, HttpStatus.valueOf(201));
+        return new ResponseEntity<MyDto.Response>((MyDto.Response) null, HttpStatus.valueOf(201));
     }
 
-    // @
     // 회원가입/로그인 - 자체 로그인
     @PostMapping("/login")
     @ApiOperation(value = "자체 로그인", response = MyDto.Response.class)
@@ -186,7 +148,6 @@ public class UserController {
         return ResponseEntity.ok().headers(resHeader).body(myRes);
     }
 
-    // @
     @GetMapping(value="/{userId}")
     @ApiOperation(value = "회원 정보 조회")
     public ResponseEntity<UserDto.Response> userInfo(@PathVariable Integer userId, HttpServletRequest request){
@@ -208,7 +169,6 @@ public class UserController {
 //        return null;
 //    }
 
-    // @
     // 회원가입/로그인 - 프로필 수정        // flag : 0:유지 , 1:수정, 2:삭제
     // @RequestBody 는 Json type으로 들어오는 객체를 파싱하는 역할 -> formData 형식에서는 사용치 않아야한다.
     @PostMapping("/{userId}")
@@ -221,14 +181,12 @@ public class UserController {
         }
         else return ResponseEntity.status(400).build();
 
-//        request.setAttribute("userId", userId);               // 테스트용
         MyDto.Response myRes = profileService.updateProfile(myReq, request);
         if( myRes == null ) return new ResponseEntity("알 수 없는 에러라서 백엔드에 다시 요청!", HttpStatus.valueOf(401));
 
         return ResponseEntity.ok().body(myRes);
     }
 
-    // @
     // 회원가입/로그인 - 비밀번호 변경
     @PatchMapping("/{userId}")
     @ApiOperation(value = "비밀번호 변경")
@@ -244,18 +202,15 @@ public class UserController {
         return new ResponseEntity(HttpStatus.valueOf(200));
     }
 
-    // @
     // 회원가입/로그인 - 회원 탈퇴
     @DeleteMapping("")
     @ApiOperation(value = "회원탈퇴")
     public ResponseEntity deleteUser(HttpServletRequest request) {
         userService.deleteUser((int) request.getAttribute("userId"));
-//        userService.deleteUser(userId);                            // 테스트용(Auto Interceptor WebConfig 적용 전)
 
         return new ResponseEntity(HttpStatus.valueOf(204));
     }
 
-    // @
     // 회원 프로필 - 1인 게시물(피드) 리스트 조회
     // 피드 게시물 리스트 조회
     @GetMapping("/{userId}/feeds")
@@ -270,7 +225,6 @@ public class UserController {
         return new ResponseEntity<FeedDto.ResponseList>(feedResList, HttpStatus.OK);
     }
 
-    // @
     // 회원의 게시물(피드) 수
     @GetMapping("/{userId}/feeds/cnt")
     @ApiOperation(value = "회원의 피드 수")
@@ -283,7 +237,6 @@ public class UserController {
         return new ResponseEntity<Map<String, Integer>>(map, HttpStatus.OK);
     }
 
-    // @
     // 서재/북카트 내 도서 검색
     @GetMapping("/{userId}/bookshelves")
     @ApiOperation(value = "북카트/서재 내 도서 검색", response = BookDto.Response.class)
@@ -297,7 +250,6 @@ public class UserController {
         return ResponseEntity.ok().body(bookResList);
     }
 
-    // @
     // 서재/북카트 책 추가
     @PostMapping("/{userId}/bookshelves")
     @ApiOperation(value = "북카트/서재 도서 추가")
@@ -310,7 +262,6 @@ public class UserController {
         return new ResponseEntity(HttpStatus.valueOf(201));
     }
 
-    // @
     // 읽은/읽을 책 수 반환
     @GetMapping("/{userId}/bookshelves/cnt")
     @ApiOperation(value = "읽은/읽을(서재/북카트) 도서 수", response = BookShelfCntDto.Response.class)
@@ -321,7 +272,6 @@ public class UserController {
         return new ResponseEntity<BookShelfCntDto.Response>(bookService.getUserReadCnt(userId), HttpStatus.OK);
     }
 
-    // @
     // 서재 책 1권 삭제하기
     @DeleteMapping("/{userId}/bookshelves/{bookId}")
     @ApiOperation(value = "서재/북카트 도서 1권 삭제하기")
@@ -334,7 +284,6 @@ public class UserController {
         return ResponseEntity.status(204).build();
     }
 
-    // @
     // 북카트에서 서재로 옮기기
     @PatchMapping("/{userId}/bookshelves/{bookId}")
     @ApiOperation(value = "북카트에서 서재로 도서 이동")
@@ -348,8 +297,6 @@ public class UserController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    // @
-    // /users/2/top-bar
     // 상단 바 도서 목록 조회
     @GetMapping("/{userId}/top-bar")
     @ApiOperation(value = "상단 바 도서 목록 조회", response = BookDto.Response.class)
@@ -361,7 +308,6 @@ public class UserController {
         return new ResponseEntity<BookDto.ResponseList>(bookResList, HttpStatus.OK);
     }
 
-    // @
     // 상단 바 도서 등록 : 서재에 있는 책인지 확인 후 등록(PRI KEY 조건으로 데이터 중복 시 throw 500)
     @PostMapping("/{userId}/top-bar")
     @ApiOperation(value = "상단 바 도서 등록")
@@ -374,7 +320,6 @@ public class UserController {
         return ResponseEntity.status(201).body(bookReq.getId());
     }
 
-    // @
     // 상단 바 도서 전체 삭제
     @DeleteMapping("/{userId}/top-bar")
     @ApiOperation(value = "상단 바 도서 전체 삭제")
@@ -387,7 +332,6 @@ public class UserController {
         return new ResponseEntity(HttpStatus.valueOf(204));
     }
 
-    // @
     // 상단바 도서 삭제
     @DeleteMapping("/{userId}/top-bar/{bookId}")
     @ApiOperation(value = "상단 바 도서 1권 삭제")
@@ -400,8 +344,6 @@ public class UserController {
         return new ResponseEntity(HttpStatus.valueOf(204));
     }
 
-    // @
-    // /users/2/follow-recommend
     // 친구 추천 목록 조회
     @GetMapping("/{userId}/follow-recommend")
     @ApiOperation(value = "친구 추천 목록 조회", response = UserDto.Response.class)
@@ -414,8 +356,6 @@ public class UserController {
         return new ResponseEntity<UserDto.ResponseList>(userResList, HttpStatus.OK);
     }
 
-    // @
-    // /users/1/keyword-recommend
     // 추천 키워드 목록
     @GetMapping("/{userId}/keyword-recommend")
     @ApiOperation(value = "추천 키워드 리스트", response = KeywordDto.Response.class)
