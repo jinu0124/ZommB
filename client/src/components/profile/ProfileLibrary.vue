96e6d36311ab9fa6fa869b779b8d6642ab9b32fe
박재준 <wowns30@naver.com>
2021-08-06 18:38:43 +0900

    Design : Profile(게시물,서재,북카트) 컴포넌트 마무리)

--- client/src/components/profile/ProfileLibrary.vue
+++ client/src/components/profile/ProfileLibrary.vue
@@ -1,15 +1,93 @@
 <template>
   <div>
-    서재다 서재
+    <div class="collection-title">
+      <b
+        >{{ nickname }}'s Collection
+        <button class="btn-5 btn-primary1">컬렉션 추가</button>
+      </b>
+    </div>
+    <div class="collection-body">
+      <ul class="collection-list">
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+        <img class="book my-3" src="@/assets/image/test/bookTest.jpg" alt="" />
+      </ul>
+    </div>
+    <span class="library-title">
+      <b>{{ nickname }}님이 가장 많이 읽은 장르는 {{ genre }} 입니다!! </b>
+      <button class="btn-5 btn-primary1">읽은책 추가</button>
+    </span>
+    <div>
+      <input
+        class="book-search-input"
+        type="text"
+        placeholder="책 제목, 작가명으로 읽은 책을 검색해보세요!"
+      />
+    </div>
+    <BookListItem />
   </div>
 </template>
 
 <script>
+import BookListItem from "@/components/BookListItem.vue";
+
 export default {
-  name: 'ProfileLibrary',
-}
+  name: "ProfileLibrary",
+  components: {
+    BookListItem,
+  },
+  data() {
+    return {
+      nickname: "Nickname",
+      genre: "소설",
+    };
+  },
+  methods: {},
+};
 </script>
 
+<style src="@/assets/style/button.css"></style>
 <style scoped>
-
+.collection-title {
+  margin: 5px 0;
+}
+ul {
+  width: 408px;
+  overflow-x: scroll;
+  white-space: nowrap;
+}
+.book {
+  height: 150px;
+  width: 100px;
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px 3px rgba(0, 0, 0, 0.25);
+  margin-right: 10px;
+}
+.library-title {
+  display: flex;
+}
+.book-search-input {
+  width: 350px;
+  height: 35px;
+  margin-top: 10px;
+  background-color: #f1f1f1;
+  color: #212121;
+  box-shadow: none;
+  border: none;
+  outline: none;
+  height: 35px;
+  padding-left: 20px;
+  font-size: 0.875rem;
+  border-radius: 20px;
+  transition: none;
+}
 </style>
--- client/src/views/feed/Feed.vue
+++ client/src/views/feed/Feed.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="feed">
-    <Header/>
     <FeedList/>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue';
 import FeedList from '@/components/feeds/feed/FeedList.vue';
 
 export default {
   name: 'Feed',
   components: {
     FeedList,
-    Header,
   },
   data: () => {
     return {
@@ -30,10 +27,5 @@ export default {
 </script>
 
 <style scoped>
-.feed{
-  display: flex;
-  flex-flow: column;
-  margin: 60px auto 0;
-  align-items: center;
-}
+
 </style>
