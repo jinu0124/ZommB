82cfbe4298a9c6eed316af9fb2a85dc26ad6ae38
이세라 <you@example.com>
2021-08-10 17:57:55 +0900

    [S05P13A602-222] Feat: 챌린지 파트 api 연결 및 기본 데이터 세팅

--- client/src/store/modules/challenge.js
+++ client/src/store/modules/challenge.js
@@ -8,32 +8,38 @@ const state = {
   weeklyBook: null,
   dailyId: null,
   dailyKeyword: null,
-  myChallenge: null
+  myChallenge: null,
+  bookmark: ['BRONZE', 'SILVER', 'GOLD', 'PLATINNUM', 'DIAMOND'],
+  pen: ['BRONZE', 'SILVER', 'GOLD', 'DIAMOND'],
 }
 
 const actions = {
   async getWeeklyBook ({ commit, dispatch }, data) {
     await challengeApi.getWeeklyBook(data)
       .then((res) => {
-        console.log(res)
-        commit('SET_WEEKLY_ID', res.data.weeklyId)
-        commit('SET_WEEKLY_BOOK_ID', res.data.bookId)
-        dispatch('getWeeklyBookInfo', res.data.bookId)
+        // console.log(res)
+        dispatch('getWeeklyBookInfo', res.data.data.bookId)
+        commit('SET_WEEKLY_ID', res.data.data.weeklyId)
+        commit('SET_WEEKLY_BOOK_ID', res.data.data.bookId)
       })
   },
   async getWeeklyBookInfo ({ commit }, data) {
     await bookApi.getBookDetail(data)
       .then((res) => {
-        console.log(res)
-        commit('SET_WEEKLY_BOOK', res.data)
+        // console.log(res)
+        commit('SET_WEEKLY_BOOK', res.data.data)
       })
   },
   async getDailyKeyword ({ commit }, data) {
     await challengeApi.getDailyKeyword(data)
       .then((res) => {
-        console.log(res)
-        commit('SET_DAILY_ID', res.data.dailyId)
-        commit('SET_DAILY_KEYWORD', res.data.keyword)
+        // console.log(res)
+        commit('SET_DAILY_ID', res.data.data.dailyId)
+        const keywordInfo = {
+          id: res.data.data.keywordId,
+          word: res.data.data.word
+        }
+        commit('SET_DAILY_KEYWORD', keywordInfo)
       })
   },
   async getMyChallenge ({ commit }, data) {
@@ -67,7 +73,20 @@ const mutations = {
 }
 
 const getters = {
-
+  penLevel: (state) => {
+    if(state.myChallenge) {
+      const cnt = state.myChallenge.dailyParticipate
+      if (cnt < 5) {
+        return 0
+      } else if (cnt < 10) {
+        return 1
+      } else if (cnt < 15) {
+        return 2
+      }
+      return 3
+    }
+    return 0
+  }
 }
 
 export default {
