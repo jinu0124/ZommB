115954e990015cd74dd936c00e83fc6c35da0443
이세라 <you@example.com>
2021-08-15 15:34:09 +0900

    Feat: 알림 갯수 표시

--- client/src/components/HeaderUserMenu.vue
+++ client/src/components/HeaderUserMenu.vue
@@ -1,6 +1,13 @@
 <template>
   <ul v-if="isLogin" class="dropdown-menu" aria-labelledby="UserMenuDropdown">
     <li><router-link class="dropdown-item" :to="{ name: 'Profile' }">내 프로필</router-link></li>
+    <li>
+      <router-link class="dropdown-item" :to="{ name: 'Notification' }">
+        <span>알림</span>
+        <span v-if="notiCnt" class="alert-cnt badge rounded-pill">{{ notiCnt }}</span>
+        <span v-else class="alert-0 badge rounded-pill">0</span>
+      </router-link>
+    </li>
     <li><router-link class="dropdown-item" :to="{ name: 'UpdateInfo' }">계정 관리</router-link></li>
     <li class="dropdown-item" type="button" @click="onLogout">로그아웃</li>
     <li><router-link class="dropdown-item" :to="{ name: 'Withdraw' }">회원 탈퇴</router-link></li>
@@ -13,7 +20,7 @@ import { mapState, mapActions } from "vuex"
 export default {
   name: 'HeaderUserMenu',
   computed: {
-    ...mapState('user', ['myInfo', 'isLogin'])
+    ...mapState('user', ['myInfo', 'isLogin', 'notiCnt'])
   },
   methods: {
     ...mapActions('user', ['onLogout']),
@@ -36,6 +43,18 @@ export default {
     color: #fff;
     font-size: 0.9rem;
     font-weight: 300;
+  }
+  .alert-cnt {
+    margin-left: 4px;
+    font-size: 0.6em;
+    padding: 3px 5px 4px;
+    background: #FF7777;
+  }
+  .alert-0 {
+    margin-left: 4px;
+    font-size: 0.6em;
+    padding: 3px 5px 4px;
+    background: #C4C4C4;
   }
   .dropdown-item:hover, .dropdown-item:focus {
     color: #fff;
