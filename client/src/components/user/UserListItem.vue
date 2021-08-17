0e389ae9339beb83fc4a3ec2044f87e0a12b7499
이세라 <you@example.com>
2021-08-17 14:17:05 +0900

    Fix: 팔로우 동작 개선

--- client/src/components/user/UserListItem.vue
+++ client/src/components/user/UserListItem.vue
@@ -45,12 +45,13 @@ export default {
     }
   },
   methods: {
-    ...mapActions('user', ['getFollowing']),
+    ...mapActions('user', ['getFollowing', 'getFollower']),
     async follow () {
       this.isFollow = true
       await userApi.follow(this.user.id)
         .then(() => {
           this.getFollowing(this.$route.params.id)
+          this.getFollower(this.$route.params.id)
         })
     },
     async unfollow () {
@@ -58,6 +59,7 @@ export default {
       await userApi.unfollow(this.user.id)
         .then(() => {
           this.getFollowing(this.$route.params.id)
+          this.getFollower(this.$route.params.id)
         })
     }
   },
