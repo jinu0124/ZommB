5a6a4d5a8fca82bec6c15cdc4cadb495271d7b0a
jinu0124 <jinwoo6612@naver.com>
2021-08-19 01:57:25 +0900

    Fix

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,8 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-messaging.js')
 
-import firebase from 'firebase/app'
-
 // firebase 서버 연동을 위한 Config
 var firebaseConfig = {
   apiKey: "AIzaSyBrOa-v9yfOHgAmdf3k1IcaRoEEatKKQLc",
@@ -13,8 +11,8 @@ var firebaseConfig = {
   appId: "1:630258256989:web:2b1332259e75b71d0091b4"
 };
 
-const app = null;
-const messaging = null;
+let app = null;
+let messaging = null;
 if (firebase.messaging.isSupported()) {
   app = firebase.initializeApp(firebaseConfig)
   messaging = firebase.messaging()
